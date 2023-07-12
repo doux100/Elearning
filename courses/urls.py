81fb65b3990809c906d8d17f9d80f2a9e4e3f796
doux100 <93144348+doux100@users.noreply.github.com>
2023-07-12 19:03:05 +0300

    enrollment done

--- courses/urls.py
+++ courses/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('list', views.courses_list, name='courses_list'),
     path('<str:slug>', views.course_desc, name='course_desc'),
     path('<str:slug>/enroll', views.courses_enroll, name='courses_enroll'),
-    path('cou/enrolled', views.enroll_done, name='enroll_done'),
+    path('courses/enrolled', views.enroll_done, name='enroll_done'),
 ]
